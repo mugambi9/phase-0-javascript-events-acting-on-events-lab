ed984ea05fce9ea46b15cceea23d6c241bc02bbd
mugambi484 <123549183+mugambi484@users.noreply.github.com>
2024-01-09 22:27:32 +0300

    assignment completed

--- index.js
+++ index.js
@@ -1,1 +1,22 @@
-// Your code here
+let dodger;
+
+function moveDodgerLeft() {
+  
+  let left = parseInt(dodger.style.left) || 0;
+
+  
+  left = Math.max(left - 5, 0); 
+
+  dodger.style.left = `${left}px`;
+}
+
+function moveDodgerRight() {
+
+  let left = parseInt(dodger.style.left) || 0;
+
+  
+  left = Math.min(left + 5, 360); 
+
+
+  dodger.style.left = `${left}px`;
+}
